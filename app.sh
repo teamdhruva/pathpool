20fa7f5f8c4fd8a41ab2eb66fb86b205b954b2cd
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-05-19 12:54:35 +0530

    Volume

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "install" ]; then
-    podman run --name pathpool --pod pathproxy-pod --replace -d -v $HOME/dhRuVa/pathpool/nginx.conf:/etc/nginx/conf.d/default.conf:Z docker.io/library/nginx
+    podman run --name pathpool --pod pathproxy-pod --replace -d -v $HOME/dhRuVa/pathpool/src:/var/www/html:Z -v $HOME/dhRuVa/pathpool/nginx.conf:/etc/nginx/conf.d/default.conf:Z docker.io/library/nginx
 elif [ "$1" == "stop" ]; then
     podman stop pathpool
 elif [ "$1" == "start" ]; then
